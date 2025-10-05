42086b83e825aa6c9e0c26c0d1acd9cae848e27e
HaroldMars <abejar199@gmailcom>
2025-10-05 13:45:21 +0800

    Happy Teacher's Day

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+        <div class="wave-container text-center mt-50 glass">
+    <h1 class="wave-text">
+        <span>O</span><span>P</span><span>E</span><span>N</span>
+    </h1>
+</div>
+      <div class="letter-image">
+        <div class="animated-mail ">
+          <div class="back-fold"></div>
+          <div class="letter text-center font-bold">
+            <h1>Happy Teacher's Day</h1>
+            <div class="letter-border"></div>
+            <div class="letter-title"></div>
+            <div class="letter-context flex">
+                <h1 className="pl-6 text-2xl">🎉</h1>
+                <h1 className="text-2xl">❤️</h1>
+                <h1 className=" text-2xl">🥳</h1>
+            </div>
+            <div class="letter-stamp">
+              <div class="letter-stamp-inner"></div>
+            </div>
+          </div>
+          <div class="top-fold"></div>
+          <div class="body"></div>
+          <div class="left-fold"></div>
+        </div>
+        <div class="shadow"></div>
+      </div>
+      </div>
+
+      
+  );
+}
